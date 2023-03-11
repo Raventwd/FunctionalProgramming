81c06fec1bd21850257f6dc26dc966647e1d4ecb
raventwd <maximowlmaster@mail.ru>
2023-03-11 20:45:45 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import java.util.*;
+
 
 
 
@@ -6,7 +8,21 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст");
+        textWordCounter(scanner);
 
+    }
+    public static void textWordCounter(Scanner scanner){
+        String string = scanner.nextLine();
+        Map<String, Integer> wordCounts = new LinkedHashMap<>();
+
+        Arrays.stream(string.split(" "))
+                .forEach((String w) -> wordCounts.put(w,wordCounts.getOrDefault(w,0)+1));
+        System.out.println("слов в тексте: "+wordCounts.size());
+        wordCounts.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(System.out::println);
+    }
 }
